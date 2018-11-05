fe260a46c1d40d3b4b74ffa6e22e703c1d945ea4
crownwangguan <crown.wg@outlook.com>
2018-11-04 22:06:21 -0500

    able to use accumulator, jdbc write but still don't know how to write to jdbc streamingly

--- us_sales_kafka_stream.py
+++ us_sales_kafka_stream.py
@@ -5,6 +5,7 @@ import json
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.streaming import StreamingContext
+import pyspark.sql.functions as sf
 from pyspark.streaming.kafka import KafkaUtils
 
 if __name__ == "__main__":
@@ -12,12 +13,18 @@ if __name__ == "__main__":
 	sc.setLogLevel("WARN")
 	sqlContext = SQLContext(sc)
 	ssc = StreamingContext(sc, 3)
-
-	kafkaStream = KafkaUtils.createStream(ssc, 'localhost:2181', "use_a_separate_group_id_for_each_stream", {'use-case-garment_sales':1}, {"auto.offset.reset": "smallest"})
+	accum = sc.accumulator(0)
+	kafkaStream = KafkaUtils.createStream(ssc, 'localhost:2181', "use_a_separate_group_id_for_each_stream", {'use-case-garment_sales':1}, {"auto.offset.reset": "largest"})
 	lines = kafkaStream.map(lambda x: json.loads(x[1]))
-	time_dstream = lines.map(lambda tweet: tweet['payload'])
-	time_dstream.pprint()
+	orderValue_dstream = lines.map(lambda tweet: tweet['payload']['ORDER_VALUE'])
+	
+	orderValue_dstream.map(lambda x: accum.add(x))
+	print(accum.value)
 	df = sqlContext.read.format("jdbc").options(url="jdbc:mysql://10.0.2.15:3306/exec_reports",driver = "com.mysql.jdbc.Driver",dbtable = "exec_summary",user="cloudera",password="cloudera").load()
 	print(df.head())
+	df = df.withColumn('SALES', sf.lit(accum.value))
+	print(df.head())
+	df.write.jdbc(url="jdbc:mysql://10.0.2.15:3306/exec_reports?user=cloudera&password=cloudera",table = "exec_summary", mode='overwrite', properties={"driver": 'com.mysql.jdbc.Driver'})
+	orderValue_dstream.pprint()
 	ssc.start()  
 	ssc.awaitTermination()
